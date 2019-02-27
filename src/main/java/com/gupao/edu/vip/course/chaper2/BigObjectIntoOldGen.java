1fcdfdada1d517576ab889bf249a549da64920cd
gouyan <2637178209@qq.com>
2019-02-27 14:32:38 +0800

    gx

--- src/main/java/com/gupao/edu/vip/course/chaper2/BigObjectIntoOldGen.java
+++ src/main/java/com/gupao/edu/vip/course/chaper2/BigObjectIntoOldGen.java
@@ -1,9 +1,7 @@
 package com.gupao.edu.vip.course.chaper2;
 
 public class BigObjectIntoOldGen {
-
     public static void main(String[] args) {
         byte[] d1 = new byte[6 * 1024 * 1024];
-
     }
 }
--- src/main/java/com/gupao/edu/vip/course/chaper2/EdenAllocator.java
+++ src/main/java/com/gupao/edu/vip/course/chaper2/EdenAllocator.java
@@ -3,6 +3,7 @@ package com.gupao.edu.vip.course.chaper2;
 public class EdenAllocator {
 
     public static void main(String[] args) {
+        /**创建 data对象，占10M 堆内存空间*/
         byte[] data = new byte[20 * 1024 * 1024];
 
     }
