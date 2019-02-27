4a44be33b68b5811e6ad9b4ea2dc45944cbb9d9c
gouyan <2637178209@qq.com>
2019-02-27 23:05:14 +0800

    gx

--- src/main/java/com/gupao/edu/vip/course/chaper2/TryFinally.java
+++ src/main/java/com/gupao/edu/vip/course/chaper2/TryFinally.java
@@ -12,12 +12,12 @@ public class TryFinally {
       stack=1, locals=3, args_size=0
          0: ldc #34  // String Hello  //去常量池 加载 常量 "Hello"
          2: astore_0  //存到 0号变量 str
-         3: aload_0  //把 0号变量 str 压栈
-         4: astore_2  //2号变量去接 return的值
+         3: aload_0  //把 0号变量 str 入栈
+         4: astore_2  //存到 2号变量 str
          5: ldc #36  // String gupao  //去常量池 加载 常量 "gupao"
          7: astore_0  //存到 0号变量 str
-         8: aload_2
-         9: areturn
+         8: aload_2  //把 2号变量 str 入栈
+         9: areturn  //返回 2号变量 str
         10: astore_1
         11: ldc #36  // String gupao  //去常量池 加载 常量 "gupao"
         13: astore_0  //存到 0号变量 str
